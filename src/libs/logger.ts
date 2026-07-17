d475f242ec22598d96c5baa636467ad73d44a0d6
berzz26 <aumtamboli2233@gmail.com>
2026-07-17 04:36:44 +0000

    feat: add Loki logging configuration and update logger initialization

--- src/libs/logger.ts
+++ src/libs/logger.ts
@@ -1,10 +1,12 @@
 import { createLogger } from "@computebay/observability";
+import { Logger } from "pino";
 
-const logger = createLogger({
+const logger : Logger = createLogger({
   serviceName: process.env.OTEL_SERVICE_NAME ?? "job-service",
   serviceVersion: process.env.SERVICE_VERSION ?? "1.0.0",
   environment: process.env.NODE_ENV ?? "development",
   logLevel: process.env.LOG_LEVEL ?? "info",
+  lokiUrl: process.env.LOKI_URL,
 });
 
 export { logger };
